9b4833a567e2755e595b7545f19937118af024c3
Aleks <aleksandrkorzevskij277@gmail.com>
2023-08-16 17:54:59 -0400

    first commit

---
+++ Program1.cs
@@ -0,0 +1,98 @@
+﻿/* int max = 0;
+int min = 0;
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if(num1 > num2)
+{
+    max = num1;
+    min = num2;
+}
+else
+{
+    max = num1;
+    min = num2;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
+*/
+
+/* int max = 0;
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+if(num1 > max)
+{
+    max = num1;
+}
+if(num2 > max)
+{
+    max = num2;
+}
+if(num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine("max = " + max);
+*\
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
+*/
+
+/*Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int remDiv = number % 2;
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+*/
+
+
+
+/*Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
+*/
